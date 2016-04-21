0b50d0552eafab572c3fe9e41382abe44a87e307
MagicHen25 <cl7416@126.com>
2016-04-21 16:54:01 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,13 @@
 #!/bin/sh
+source /etc/profile
+int=1
+while(( $int<=5 ))
+do
+        mem_total=`free -m | grep Mem: | awk '{print $2}'`
+        mem_used_small=`free -m | grep - | awk '{print $3}'`
+        mem_used_big=`free -m | grep Mem: | awk '{print $3}'`
+        cpu_idle=`mpstat | sed -n '4p' | awk '{print $12}'`
+        sleep 9
+        echo -e "${mem_used_small},${mem_used_big},${mem_total},${cpu_idle}" >>/home/rtt/chenl/cpumem/out.csv
+        let "int++"
+done
