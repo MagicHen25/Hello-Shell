ce1ba49ed293ec01ad5e9e3d9500406b1ea9aad1
MagicHen25 <cl7416@126.com>
2016-05-04 11:43:53 +0800

    Update checkout.sh

--- checkout.sh
+++ checkout.sh
@@ -2,6 +2,7 @@
 source /etc/profile
 IP=`ifconfig | grep -oE "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | sed -n '1p'`
 MAIL=cl7416@126.com
+LOG_FILE=/home/rtt/chenl/checkout/check.log
 cd /home/rtt/application/RTT_DAG/logs
 [ `ps -ef | grep -c RTT_DAG ` -ne 2 ] && echo -e "IP:${IP}\n找不到网关进程，请检查。" | mail -s "服务器告警！IP:${IP}" $MAIL
 file=`ls -t | egrep "ssjt-info.log.[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}" | sed -n '1p'`;a=`stat -c %Y $file`;b=`date +%s`; [ $[ $b - $a ] -gt 4500 ] && echo "IP:${IP}\n服务器已经超过1小时无最新日志生成，请及时查看。" | mail -s "服务器告警！IP:${IP}" $MAIL
