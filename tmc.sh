c32289a108c004e2aee2e52bd37acea4e556323c
MagicHen25 <cl7416@126.com>
2016-05-11 17:03:24 +0800

    Update tmc.sh

--- tmc.sh
+++ tmc.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 source /etc/profile
-export LANG=zh_CN.UTF-8
+export LANG=en_US.UTF-8
 IP=`ifconfig | grep -oE "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | sed -n '1p'`
 MAIL="shidm@neusoft.com yan.sh@neusoft.com li_dl@neusoft.com gaowl@neusoft.com zhang_qiang.neu@neusoft.com jin-gd@neusoft.com chenl.long@neusoft.com"
 
 LOG_FILE=/home/rtt/chenl/checkout/check.log
 
-PATH_TMC_01=/home/rtt/application/tmc/log/
-PATH_TMC_02=/home/rtt/application/tmc_dongguan/log/
-PATH_TMC_03=/home/rtt/application/tmc_foshan/log/
+PATH_TMC_01=/home/rtt/application/tmc258/log
+PATH_TMC_02=/home/rtt/application/tmc259/log
+PATH_TMC_03=/home/rtt/application/tmc257/log
 
 [ `ps -ef | grep java | wc -l` -ne 4 ] && echo -e "IP:${IP}\n主机名:${HOSTNAME}\n日志路径:${LOG_PATH}\n故障:有TMC服务未开启！请及时查看。" | \
 mail -s "服务器告警 IP:${IP}" $MAIL && echo -e "`date`\t IP:${IP}有TMC服务未开启！请及时查看。" >> $LOG_FILE
